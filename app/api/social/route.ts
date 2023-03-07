632e744ba9a0701bafc9ee9c22584d77a34d06e8
son.tran <son.tran@team.enouvo.com>
2023-03-07 20:59:49 +0700

    feat(main): fix api social

--- app/api/social/route.ts
+++ app/api/social/route.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from 'next/server';
+
 export async function POST(request: Request) {
   const res = await fetch(`${process.env.API_URL}/interview/social`, {
     method: 'POST',
@@ -7,5 +9,5 @@ export async function POST(request: Request) {
     body: request.body,
   });
   const data = await res.json();
-  return Response.json(data);
+  return NextResponse.json(data);
 }
