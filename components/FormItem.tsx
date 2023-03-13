1b9aabe0139bf4746b722a839b7a9e4efabb3371
son.tran <son.tran@team.enouvo.com>
2023-03-13 14:59:07 +0700

    feat(main): fix accessecbility

--- components/FormItem.tsx
+++ components/FormItem.tsx
@@ -9,8 +9,11 @@ interface Props {
 const FormItem = ({ field, children, label}: Props) => {
   return (
     <div className="w-full">
-      {label ? <label className="block text-sm text-[#333333]">Description {children}</label> : children}
-      {field && <p className="text-red-500 m-0 mt-2">{field.message}</p>}
+      <label>
+        <span className="block text-sm text-[#333333]"> {label} </span>
+        {children}
+        {field && <p className="text-red-500 m-0 mt-2">{field.message}</p>}
+      </label>
     </div>
   );
 }
